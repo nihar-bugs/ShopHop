f2934826a07498d75391e3ab1daa1a29ce40084e
Nihar Makwana <niharmakwana46@gmail.com>
2023-02-02 11:46:12 +0530

    Created layout js files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+//import HomePage from "./pages/HomePage";
+import React from "react";
+import Navbar from "./components/Navbar";
 
+
+const App = () => {
+  return  (<>
+    <Navbar/>
+  </>
+  )
+  
+  }
 export default App;
