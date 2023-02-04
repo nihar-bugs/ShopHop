0456931445d791faf13470c272d9b6fed07bedf6
Nihar Makwana <niharmakwana46@gmail.com>
2023-02-04 15:16:04 +0530

    Created env variables, established db conn and launched express server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.MONGO_CONNECT_URL)
+  .then(() => console.log("DB Connected!!"))
+  .catch((err) =>
+    console.log("DB Connection Failed! Please Check. Error Message:", err)
+  );
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Backend is running on", process.env.PORT)
+);
