d06ff8a523e68dabca2daec0f7965893802c9db1
Nihar Makwana <niharmakwana46@gmail.com>
2023-02-05 12:58:35 +0530

    Login completed; env file updated for jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+// REGISTER
 
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
+
+  // PERFORMING VALIDATION
 
   if (!username) {
     res.json({
@@ -28,12 +32,13 @@ router.post("/register", async (req, res) => {
       $or: [{ email: email }, { username: username }],
     });
     if (exist) {
-      console.log("Entered 1st try");
       return res.json({ error: "Either Email or Username is taken" });
     }
   } catch (err) {
-    console.log("EXISt ME DIKKAT");
+    console.log("Error finding user:", err);
   }
+
+  // SAVING USER TO DATABASE
 
   const newUser = new User({
     username: username,
@@ -46,6 +51,54 @@ router.post("/register", async (req, res) => {
     res.json(savedUser);
   } catch (err) {
     res.json(err.toString());
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  // Fetching login credentials from incoming request
+
+  const { username, password } = req.body;
+
+  // PERFORMING VALIDATION
+
+  if (!username || username == "") {
+    return res.json({
+      error: "Name is required",
+    });
+  }
+
+  if (!password || password.length < 6 || password == "") {
+    return res.json({
+      error: "Password is required and should be atleast 6 character long",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ username: username });
+
+    if (!user) {
+      return res.status(401).json({ error: "Wrong credentials" });
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (password !== hashedPassword) {
+      return res.status(401).json({ error: "Incorrect Password" });
+    }
+
+    const authToken = jwt.sign({ id: user._id }, process.env.JWT_SEC_KEY, {
+      expiresIn: "1d",
+    });
+
+    const { noPassword, ...others } = user._doc;
+
+    res.status(200).json({ ...others, authToken });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
